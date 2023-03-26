bebfab1ef69bfb8c9a7db95d94fe06ae69f985db
Muhammed Saeed <musaeed@tony-1.coli.uni-saarland.de>
2023-03-26 22:56:06 +0200

    pretrain t5-english on new dataset;

---
+++ T5_lowresourceGeneration.py
@@ -0,0 +1,45 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+
+# 1. Gather a large corpus of text in the new language
+corpus_file = "/local/musaeed/ROBERTA-base-en/Mono_lingual_data/pcm_entire_mono.txt"
+
+# 2. Preprocess the text by cleaning and tokenizing it
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
+max_length = 512
+
+# 3. Convert the preprocessed text into a format suitable for T5 pretraining
+dataset = TextDataset(tokenizer=tokenizer, file_path=corpus_file, block_size=max_length)
+
+# 4. Train the T5 model on the preprocessed data
+model = T5ForConditionalGeneration.from_pretrained("t5-small")
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+# Define a collate function for the data collator
+def my_collate_fn(examples):
+    input_ids = [example["input_ids"] for example in examples]
+    attention_mask = [example["attention_mask"] for example in examples]
+
+    return {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask,
+    }
+
+data_collator.collate_fn = my_collate_fn
+
+training_args = TrainingArguments(
+    output_dir="/local/musaeed/new_language_t5_pretraining",
+    num_train_epochs=10,
+    per_device_train_batch_size=4,
+    logging_steps=100,
+    save_steps=5000,
+    learning_rate=3e-4,
+    report_to="wandb",
+    run_name="T5 for LowResource"
+)
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+    data_collator=data_collator
+)
+trainer.train()

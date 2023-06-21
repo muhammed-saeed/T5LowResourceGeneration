9b0cfdf5c035ed4890a98949d1d4df497ad6e5e0
Muhammed Saeed <musaeed@tony-1.coli.uni-saarland.de>
2023-06-21 03:43:21 +0200

    update the presentation.py

--- presentation/presentation.py
+++ presentation/presentation.py
@@ -2,11 +2,11 @@ import argparse
 import pandas as pd
 
 def main(args):
-    pdtb_df = pd.read_csv(args.input_file, low_memory=False)
+    pdtb_df = pd.read_csv(args.pcm_input_file, low_memory=False)
     pcm_df = pdtb_df[['ARG1_RawText_PCM', 'ARG2_RawText_PCM', 'FullRawText_PCM', 'Conn1_PCM',
        'Conn2_PCM', 'Connective_RawText_PCM']]
     pcm_df.to_csv(args.pcm_output_file)
-    en_df = pdtb_df[['ARG1_RawText', 'ARG2_RawText', 'FullRawText', 'Conn1',
+    en_df = pdtb_df[['Arg1_RawText', 'Arg2_RawText', 'FullRawText', 'Conn1',
        'Conn2', 'Connective_RawText']]
     en_df.to_csv(args.en_output_file)
     
@@ -14,7 +14,9 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(help='Creating Sample for presentation')
+    parser = argparse.ArgumentParser(description='Creating Sample for presentation')
     parser.add_argument("--pcm_input_file", type=str, default = "data/pdtb2_pcm.csv", help="Path to the pidgin PDTB ")
     parser.add_argument("--pcm_output_file", type=str, default="presentation/data/pdtb2_pcm_presentation.csv", help="Path to the output of the presentation file")
     parser.add_argument("--en_output_file", type=str, default="presentation/data/pdtb2_en_presentation.csv", help="Path to the english presentation")
+    args = parser.parse_args()
+    main(args)

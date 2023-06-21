810202aec6638a3c2989cc20cc5326f539c522c2
Muhammed Saeed <musaeed@tony-1.coli.uni-saarland.de>
2023-06-21 03:59:01 +0200

    update the presentation.py

--- presentation/presentation.py
+++ presentation/presentation.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 def main(args):
     pdtb_df = pd.read_csv(args.pcm_input_file, low_memory=False)
-    pcm_df = pdtb_df[['Relation','Conn1_PCM',  'Conn2_PCM','ARG1_RawText_PCM', 'ARG2_RawText_PCM', 'FullRawText_PCM',  'Connective_RawText_PCM']]
+    pcm_df = pdtb_df[['Relation', 'Connective_RawText_PCM', 'Conn1_PCM',  'Conn2_PCM','ARG1_RawText_PCM', 'ARG2_RawText_PCM', 'FullRawText_PCM',  'Connective_RawText_PCM']]
     pcm_df.to_csv(args.pcm_output_file)
-    en_df = pdtb_df[['Relation', 'Conn1',        'Conn2','Arg1_RawText', 'Arg2_RawText', 'FullRawText',  'Connective_RawText']]
+    en_df = pdtb_df[['Relation', 'Connective_RawText','Conn1',        'Conn2','Arg1_RawText', 'Arg2_RawText', 'FullRawText',  'Connective_RawText']]
     en_df.to_csv(args.en_output_file)
     
 

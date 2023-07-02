5686874b6ab70cea40f9947f988f615302303f8b
Muhammed Saeed <musaeed@tony-1.coli.uni-saarland.de>
2023-07-02 22:39:12 +0200

    modifiying the files

--- pdtb3Data/rawText/scripts/translateFullText.py
+++ pdtb3Data/rawText/scripts/translateFullText.py
@@ -1,6 +1,5 @@
 
 import os
-import os
 import logging
 import sacrebleu
 import pandas as pd
@@ -26,21 +25,22 @@ model_output_dir="/local/musaeed/BESTT5TranslationModel"
 model = T5Model("t5", model_output_dir, args=model_args, use_cuda=False)
 
  
-
+english_folder_path = "pdtb3Data/rawText/raw"
 # Get all folders path inside a main folder
-folder_names = [name for name in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, name))]
+folder_names = [name for name in os.listdir(english_folder_path) if os.path.isdir(os.path.join(english_folder_path, name))]
+print(folder_names)
+pidgin_parallel_path = "/local/musaeed/NaijaDiscourseClassification/pdtb3Data/rawText/pidgin"
 
-pidgin_parallel_path = "pdtb3Data/rawText/pidgin"
-
-
+encoding = "latin-1"
 
 
 # Iterate over each file path and read the contents
 for folder_path in folder_names:  
     # Get all file paths inside the folder and its subfolders
-    file_paths = get_all_files(folder_path)
+    file_paths = get_all_files(os.path.join(english_folder_path,folder_path))
     for file_path in file_paths:
-        with open(file_path, 'r') as file:
+        print(f"currently working with file {file_path}")
+        with open(file_path, 'r', encoding= encoding) as file:
             lines = file.readlines()
 
         # Remove empty lines from the content
@@ -48,9 +48,12 @@ for folder_path in folder_names:
 
         pcm_preds = model.predict(english_non_empty_lines)
 
-        folder_list = file_path.split()[-3:]
-        pcm_folder_path = folderchecker(pidgin_parallel_path, folder_list)
+        folder_list = file_path.split("/")[-5:] 
         
+        print(f'the file path split is {file_path.split("/")[-5:]}')
+        pcm_folder_path = folderchecker(pidgin_parallel_path, folder_list)
+
+        print(f"file name is {pcm_folder_path}")
         writelines(pcm_folder_path, pcm_preds)
 
 
--- pdtb3Data/rawText/scripts/utils.py
+++ pdtb3Data/rawText/scripts/utils.py
@@ -1,3 +1,4 @@
+import os
 # Recursive function to get all files inside a folder and its subfolders
 def get_all_files(folder_path):
     file_paths = []
@@ -13,16 +14,20 @@ def writelines(folder_path, lines):
     print(f"Complete the writing to the file {folder_path}")
 
 
+
 def folderchecker(folder_path_pcm, folder_list):
-    folder_path =  folder_path_pcm + '/'.join(folder_list)
+    folder_path = os.path.join(folder_path_pcm, *folder_list[:-1])
     print(folder_path)
 
-    # Check if the folder exists
-    if not os.path.exists(folder_path):
-        # Create the folder
-        os.makedirs(folder_path)
-        print("Folder created:", folder_path)
-        return folder_path
-    else:
-        print("Folder already exists:", folder_path)
-        return folder_path
+    # Create the folder path if it doesn't exist
+    os.makedirs(folder_path, exist_ok=True)
+    print("Folder created:", folder_path)
+
+    file_path = os.path.join(folder_path, folder_list[-1])
+    if not os.path.exists(file_path):
+        # Create the file
+        with open(file_path, "w") as file:
+            print("File created:", file_path)
+
+    return file_path
+
